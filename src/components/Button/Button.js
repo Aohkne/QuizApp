e43897db74aa52e55b2fdf03a12d8e4b321d9e10
KHOA <aohkne@gmail.com>
2024-10-08 10:15:33 +0700

    1-commit
    Set up

---
+++ src/components/Button/Button.js
@@ -0,0 +1,5 @@
+function Button() {
+  return <h1>hihi</h1>;
+}
+
+export default Button;
---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="wrappper">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Header;
